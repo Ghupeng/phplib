e4484fedcea968c68a262ca50e798157870ff6fa
gname <ghupeng@163.com>
2017-10-22 19:51:04 +0800

    类库

---
+++ src/Help.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: huguopeng
+ * Date: 2017/10/22
+ * Time: 19:48
+ */
+namespace phplib;
+
+class Help {
+	public function encrypt() {
+		echo "已加密秘钥";
+	}
+}
